84fa9b05e4eabb20216706b435694b21c53a0e35
ravikishore1993 <ravikishore1993@gmail.com>
2015-06-18 20:52:18 +0530

    Trting to get it running in shiny

---
+++ server.R
@@ -0,0 +1,54 @@
+# Cleans workspace
+?rm
+
+# Setting up packages on our workspace
+library('neuralnet',lib="/home/ravi/R/x86_64-pc-linux-gnu-library/3.0")
+library('shiny')
+
+
+
+# Setting directory to get the file
+setwd('.')
+directory <- getwd()
+dat <- read.csv(paste(directory,'/parkinsons.data', sep=""), header = TRUE, sep = ",")
+
+# Removing the 'name' column
+features_total <- dat[,-1]
+# Removing the status column
+features_total <- features_total[,-17]
+
+# Our training vector
+status <- dat[,18]
+
+features_train = features_total[1:150,]
+status_train = status[1:150]
+
+train <- data.frame(features_train, status_train)
+n <- names(train)
+f <- as.formula(paste('status_train ~', paste(n[!n %in% 'status_train'], collapse = ' + ')))
+
+# ***********************************
+# Making the model, some instructions
+# ***********************************
+
+# use hidden = c(x,y) to have x and y no. of neurons in 1st and 2nd hidden layers
+# tweak with the parameters for better results, use help(neuralnet) for more.
+net <- neuralnet(f, train, hidden = 10, threshold=0.01)
+
+test_features <- features_total[150:nrow(features_total),]
+status_test = status[150:length(status)]
+net.results <- compute(net, test_features)
+print(net.results$net.result)
+
+result <- (net.results$net.result > 0.5)
+
+print("Number of false reports:")
+flaws <- sum(result!=status_test)
+print(flaws)
+
+shinyServer(function(input, output) {
+ output$view <- renderPrint({
+ 	print("Foo")
+ 	})
+
+})
---
+++ ui.R
@@ -0,0 +1,11 @@
+library(shiny)
+
+# Define UI for application that plots random distributions 
+shinyUI(fluidPage(
+  
+  # Application title
+  headerPanel("Output"),
+  div("Yol")
+  
+
+))

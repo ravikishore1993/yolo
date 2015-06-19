b995f79e899e145534a5c9c43ac00f41562c58b1
anirudt <anirudt@gmail.com>
2015-06-19 14:54:42 +0530

    added feat-avger

---
+++ pymean.py
@@ -0,0 +1,30 @@
+import os
+from pandas import DataFrame
+import pandas
+import numpy as np
+
+# Listing files in the directory
+csv_files, files = [], []
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for e in files:
+    if(e.find('.csv')!=-1):
+        csv_files.append(e)
+
+# Initing dataframes
+mean_data = DataFrame()
+concated_data = DataFrame()
+
+# Listing csv files in pwd
+print csv_files
+
+# Concating all features into one
+for file in csv_files:
+    print file
+    data = DataFrame.from_csv(file, header=None)
+    mean_data = data.mean()
+    frames = [concated_data, mean_data]
+    concated_data = pandas.concat(frames) 
+
+print concated_data.shape
+
+# TODO: Piping output into a file
